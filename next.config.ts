a67bf4baab4fa1968f81081aeca1112d891c9fac
Abhay <abhay@speechgears.com>
2025-10-06 11:29:57 +0530

    build error

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export', // Enables static site generation
+  distDir: 'dist', // Sets output directory to ./dist
 };
 
 export default nextConfig;
